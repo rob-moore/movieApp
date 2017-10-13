1bba9ddaf317e02065a0b0bbb867204468d74819
Rob Moore <regfoxes@gmail.com>
2017-10-12 20:25:29 -0600

    added ui-router, get 100 movies, limit sql results

--- index.js
+++ index.js
@@ -6,62 +6,68 @@ const config = require('./config');
 const app = express();
 
 const connection = mysql.createConnection({
-    host: config.dbHost,
-    user: config.dbUser,
-    password: config.dbPass,
-    database: config.dbDatabase 
+  host: config.dbHost,
+  user: config.dbUser,
+  password: config.dbPass,
+  database: config.dbDatabase
 })
 
 app.use(serveStatic('app'));
 
 connection.connect();
 
-// Define gather endpoint
-app.get('/gather', (req, resp) => {
-  const url = `https://api.themoviedb.org/3/discover/movie?with_genres=878&sort_by=popularity.desc&certification=R&primary_release_year=2015&api_key=${config.tmdbKey}`
-  
-  // Requests data from the movie database
+const insertMovies = url => {
   request(url, function (err, res, body) {
-    if(err) {
+    if (err) {
       console.log(err)
       return false;
     }
-      const movieData = JSON.parse(res.body);
-      
-      // Ensure data is fresh
-      connection.query('TRUNCATE movies')
+    const movieData = JSON.parse(res.body);
 
-      movieData.results.forEach(element => {
-        let post = {
-          id: element.id,
-          adult: element.adult,
-          backdrop_path: element.backdrop_path,
-          genre_ids: JSON.stringify(element.genre_ids),
-          original_language: element.original_language,
-          original_title: element.original_title,
-          overview: element.overview,
-          popularity: element.popularity,
-          poster_path: element.poster_path,
-          release_date: element.release_date,
-          title: element.title,
-          video: element.video,
-          vote_average: element.vote_average,
-          vote_count: element.vote_count,
-        };
-        connection.query('INSERT INTO movies SET ?', post, (error, results, fields) => {
-          if (error) {
-            console.error(error)
-          }
-        })
-      }, this);
-      resp.send('we did the thing')
-      
+
+    movieData.results.forEach(element => {
+      let post = {
+        id: element.id,
+        adult: element.adult,
+        backdrop_path: element.backdrop_path,
+        genre_ids: JSON.stringify(element.genre_ids),
+        original_language: element.original_language,
+        original_title: element.original_title,
+        overview: element.overview,
+        popularity: element.popularity,
+        poster_path: element.poster_path,
+        release_date: element.release_date,
+        title: element.title,
+        video: element.video,
+        vote_average: element.vote_average,
+        vote_count: element.vote_count,
+      };
+      connection.query('INSERT INTO movies SET ?', post, (error, results, fields) => {
+        if (error) {
+          console.error(error)
+        }
+      })
+    }, this);
+
   })
-  
+}
+
+// Define gather endpoint
+app.get('/gather', (req, resp) => {
+  // Ensure data is fresh
+  connection.query('TRUNCATE movies')
+
+  // Gets 100 results 
+  for (let i = 1; i <= 5; i++) {
+    const url = `https://api.themoviedb.org/3/discover/movie?with_genres=878&sort_by=popularity.desc&certification=R&primary_release_year=2015&page=${i}&api_key=${config.tmdbKey}`
+    insertMovies(url);
+  }
+  resp.send('we did the thing')
+
 })
 
 app.get('/movies', (req, resp) => {
-  connection.query('SELECT * FROM movies', (error, results, fields) => {
+  connection.query('SELECT * FROM movies ORDER BY vote_count desc LIMIT 20', (error, results, fields) => {
     if (error) {
       console.error(error);
       resp.send('error dood')
@@ -77,4 +83,3 @@ app.get('/movies', (req, resp) => {
 app.listen(4200, () => {
   console.log('express is working')
 })
-
