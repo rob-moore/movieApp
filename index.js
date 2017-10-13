ff17bc167a696b1870c33a5f9a9cfbf4764fbd22
Rob Moore <regfoxes@gmail.com>
2017-10-12 19:11:06 -0600

    add writing to database and get request from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const request = require('request');
+const serveStatic = require('serve-static');
 const config = require('./config');
 const app = express();
 
@@ -10,25 +11,70 @@ const connection = mysql.createConnection({
     password: config.dbPass,
     database: config.dbDatabase 
 })
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
 
-app.get('/gather', (req, res) => {
+app.use(serveStatic('app'));
+
+connection.connect();
+
+// Define gather endpoint
+app.get('/gather', (req, resp) => {
   const url = `https://api.themoviedb.org/3/discover/movie?with_genres=878&sort_by=popularity.desc&certification=R&primary_release_year=2015&api_key=${config.tmdbKey}`
+  
+  // Requests data from the movie database
   request(url, function (err, res, body) {
-    console.log(body);
     if(err) {
       console.log(err)
       return false;
     }
+      const movieData = JSON.parse(res.body);
+      
+      // Ensure data is fresh
+      connection.query('TRUNCATE movies')
+
+      movieData.results.forEach(element => {
+        let post = {
+          id: element.id,
+          adult: element.adult,
+          backdrop_path: element.backdrop_path,
+          genre_ids: JSON.stringify(element.genre_ids),
+          original_language: element.original_language,
+          original_title: element.original_title,
+          overview: element.overview,
+          popularity: element.popularity,
+          poster_path: element.poster_path,
+          release_date: element.release_date,
+          title: element.title,
+          video: element.video,
+          vote_average: element.vote_average,
+          vote_count: element.vote_count,
+        };
+        connection.query('INSERT INTO movies SET ?', post, (error, results, fields) => {
+          if (error) {
+            console.error(error)
+          }
+        })
+      }, this);
+      resp.send('we did the thing')
+      
   })
+  
 })
 
-app.get('/display', (req, res) => {
+app.get('/movies', (req, resp) => {
+  connection.query('SELECT * FROM movies', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      resp.send('error dood')
+    }
+    console.log(results);
+    resp.send(results);
+  });
 
 })
+
+
 
 app.listen(4200, () => {
   console.log('express is working')
 })
+
